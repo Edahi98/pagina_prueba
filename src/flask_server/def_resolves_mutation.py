d2cbcf906852f61b0731f40533ab6ce3aef02e91
Edahi98 <garciaedahi3822@gmail.com>
2025-04-11 13:02:24 -0600

    Delete src directory

--- src/flask_server/def_resolves_mutation.py
+++
@@ -1,5 +0,0 @@
-from ariadne import MutationType
-from . import resolves_mutation
-
-mutation = MutationType()
-mutation.set_field("addfilter", resolves_mutation.resolve_addfilter)
--- src/flask_server/type_def_const.py
+++
@@ -1,8 +0,0 @@
-TYPE_DEF = """
-    type Query {
-        _empty: String
-    }
-    type Mutation{
-        addfilter(bs64: String!, filter: String!, format_image: String!): String!
-    }
-"""
